380e10d2f84d3ef5f7c2576d92266d51223347bf
ALBAB404 <imalbab4@gmail.com>
2024-10-17 18:59:22 +0600

    category create done

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Exception;
 use App\Models\Category;
 use Illuminate\Support\Str;
+use App\Manager\ImageUploadManager;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
-use App\Manager\ImageUploadManager;
 
 class CategoryController extends Controller
 {
@@ -20,24 +21,32 @@ class CategoryController extends Controller
 
     public function store(CategoryRequest $request)
     {
-        info($request->all());
-        $category = $request->except('photo');
-        $category['user_id'] = auth()->id();
-        $category['slug'] = Str::slug($request->input('slug'));
-        if ($request->has('photo')) {
-            $file             = $request->input('photo');
-            $height           = 450;
-            $width            = 450;
-            $height_thumb     = 150;
-            $width_thumb      = 150;
-            $name             = Str::slug($request->input('slug'));
-            $path = 'images/uploads/category/';
-            $path_thumb = 'images/uploads/category_thumb/';
-            $category['photo'] = ImageUploadManager::uploadImage($name, $width, $height, $path, $file);
-            ImageUploadManager::uploadImage($name, $width_thumb, $height_thumb, $path_thumb, $file);
-        }
+        try {
+            $category = $request->except('photo');
+            $category['user_id'] = auth()->id();
+            $category['slug'] = Str::slug($request->input('slug'));
+            if ($request->has('photo')) {
+                $file             = $request->input('photo');
+                $height           = 450;
+                $width            = 450;
+                $height_thumb     = 150;
+                $width_thumb      = 150;
+                $name             = Str::slug($request->input('slug'));
+                $path = 'images/uploads/category/';
+                $path_thumb = 'images/uploads/category_thumb/';
+                $category['photo'] = ImageUploadManager::uploadImage($name, $width, $height, $path, $file);
+                ImageUploadManager::uploadImage($name, $width_thumb, $height_thumb, $path_thumb, $file);
+            }
 
-        (new Category)->storeCategory($category);
+            (new Category)->storeCategory($category);
+            return response()->json(['message' => 'Category Created Successfully', 'cls' => 'success']);
+        } catch (Exception $e) {
+            // Return a JSON response with error details
+            return response()->json([
+                'message' => 'Category creation failed',
+                'error' => $e->getMessage()
+            ], 500); // 500 indicates a server-side error
+        }
     }
 
 
