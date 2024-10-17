38b064a2d2c867ff09de870ab60c434d2e7b4c1b
ALBAB404 <imalbab4@gmail.com>
2024-10-17 15:45:23 +0600

    image intervention er maddhome category image inserted done

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
+use App\Manager\ImageUploadManager;
 
 class CategoryController extends Controller
 {
@@ -19,15 +20,21 @@ class CategoryController extends Controller
 
     public function store(CategoryRequest $request)
     {
+        info($request->all());
         $category = $request->except('photo');
         $category['user_id'] = auth()->id();
         $category['slug'] = Str::slug($request->input('slug'));
         if ($request->has('photo')) {
-            $height       = 450;
-            $width        = 450;
-            $height_thumb = 450;
-            $width_thumb  = 450;
-            $path         = Str::slug($request->input('slug'));
+            $file             = $request->input('photo');
+            $height           = 450;
+            $width            = 450;
+            $height_thumb     = 150;
+            $width_thumb      = 150;
+            $name             = Str::slug($request->input('slug'));
+            $path = 'images/uploads/category/';
+            $path_thumb = 'images/uploads/category_thumb/';
+            $category['photo'] = ImageUploadManager::uploadImage($name, $width, $height, $path, $file);
+            ImageUploadManager::uploadImage($name, $width_thumb, $height_thumb, $path_thumb, $file);
         }
 
         (new Category)->storeCategory($category);
--- app/Manager/ImageUploadManager.php
+++ app/Manager/ImageUploadManager.php
@@ -2,10 +2,26 @@
 
 namespace App\Manager;
 
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
 class ImageUploadManager {
-    public function uploadImage ($name, $width, $height, $path, $file)
+    final public static function uploadImage ($name, $width, $height, $path, $file)
     {
-        $image_file_name = $name.'.webp';
-        Image::make($file)->fit($width, $height)->save(public_path($path).$image_file_name, quality: 50, 'webp');
+        $manager = new ImageManager(new Driver());
+        // ইমেজ তৈরি করুন
+        $image_file_name = $name . '.webp';
+        $img = $manager->read($file);
+        $img = $img->resize($width, $height);
+        $img->toWebp()->save(public_path($path) . $image_file_name);
+        return $image_file_name;
+    }
+
+    final public static function deletePhoto ($path, $img):void
+    {
+        $path = public_path($path).$img;
+        if ($img != '' && file_exists($path)) {
+            unlink($path);
+        }
     }
 }
